61d5068ab6baffe862a39d0d7d71c3996c7a76db
Chandra Kumar <chandu.cky@gmail.com>
2016-03-13 18:22:20 +0530

    first cimmit

---
+++ springmvc/src/main/java/com/cky/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,20 @@
+package com.cky.springmvc.dao;
+
+
+import java.util.List;
+ 
+import com.cky.springmvc.model.Employee;
+ 
+public interface EmployeeDao {
+ 
+    Employee findById(int id);
+ 
+    void saveEmployee(Employee employee);
+     
+    void deleteEmployeeBySsn(String ssn);
+     
+    List<Employee> findAllEmployees();
+ 
+    Employee findEmployeeBySsn(String ssn);
+ 
+}
