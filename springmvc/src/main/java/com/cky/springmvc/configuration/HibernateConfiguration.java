e8f493a847c715faa898353d3035ecc3e2b6086d
Chandra <chandu.cky@gmail.com>
2016-07-10 15:25:05 +0530

    changes in configuration

--- springmvc/src/main/java/com/cky/springmvc/configuration/HibernateConfiguration.java
+++ springmvc/src/main/java/com/cky/springmvc/configuration/HibernateConfiguration.java
@@ -1,9 +1,9 @@
 package com.cky.springmvc.configuration;
 
 import java.util.Properties;
- 
+
 import javax.sql.DataSource;
- 
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -36,12 +36,14 @@ public class HibernateConfiguration {
      
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
         dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
         dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
         dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        
         return dataSource;
+        
     }
      
     private Properties hibernateProperties() {
@@ -49,6 +51,9 @@ public class HibernateConfiguration {
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        System.out.println(" environment.getRequiredProperty(hibernate.hbm2ddl.auto) = " + environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        
         return properties;        
     }
      
